07522f6c2f7e5110d907952e4c43ed61480cc6b7
Hemanth Kini <hemanth.kini@gmail.com>
2013-09-06 23:46:39 -0400

    initial nuancey stuff

--- MorningOrganizer/src/com/pennapps/morningorganizer/MainActivity.java
+++ MorningOrganizer/src/com/pennapps/morningorganizer/MainActivity.java
@@ -1,12 +1,13 @@
 package com.pennapps.morningorganizer;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 /*test comment*/
 // penis
 public class MainActivity extends Activity {
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
--- MorningOrganizer/src/com/pennapps/morningorganizer/Nuance.java
+++ MorningOrganizer/src/com/pennapps/morningorganizer/Nuance.java
@@ -1,5 +1,43 @@
 package com.pennapps.morningorganizer;
 
+import android.content.Context;
+
+import com.nuance.nmdp.speechkit.SpeechKit;
+import com.nuance.nmdp.speechkit.Vocalizer;
+
 public class Nuance {
 
+	public static final String TTS_KEY = "com.nuance.nmdp.sample.tts";	 //to change later?
+    
+    private Vocalizer _vocalizer;
+    private Object _lastTtsContext = null;
+    
+	
+	private static SpeechKit _speechKit;
+    
+    // Allow other activities to access the SpeechKit instance.
+    static SpeechKit getSpeechKit()
+    {
+        return _speechKit;
+    }
+    
+    static void initializeSpeechKit(Context appContext)
+    {
+    	if (_speechKit == null)
+    	{
+    		  _speechKit = SpeechKit.initialize((Context) appContext, NuanceAppInfo.SpeechKitAppId, NuanceAppInfo.SpeechKitServer, NuanceAppInfo.SpeechKitPort, NuanceAppInfo.SpeechKitSsl, NuanceAppInfo.SpeechKitApplicationKey);
+               _speechKit.connect();
+    	}
+    	
+    }
+    
+    static void closeSpeechKit()
+    {
+    	if (_speechKit != null)
+    	{
+    		_speechKit.cancelCurrent();
+    		_speechKit.release();
+    	}
+    }
+	
 }
